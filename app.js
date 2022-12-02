81a7c2c14c77415f7a4b0d03775fe22b540791a2
lauradiaz <lauradiaz1586@gmail.com>
2022-12-02 11:16:37 +0400

    fix typo

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const auth = require("./middleware/authentication");
 app.get("/", (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // routes
 app.use("/api/v1/auth", authRouter);
